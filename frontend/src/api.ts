75dae17f5ba2593c61435d9532b9c9dde392036c
JodhbirS <jodhtmbusiness@gmail.com>
2025-06-22 00:03:54 -0400

    made api have relative path

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,5 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "";
 export async function getFights() {
-  const res = await fetch(`${BACKEND_URL}/fights`);
+  const res = await fetch('/fights');
   if (!res.ok) throw new Error("Failed to fetch /fights");
   return await res.json();
 }
