ccd09038b9665e4b5e9317f5223c459082114e7e
JodhbirS <jodhtmbusiness@gmail.com>
2025-09-11 00:03:19 -0400

    update cors

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://frontend:5173",
-        "https://*.vercel.app",
+        "https://fightev.vercel.app",
         "https://vercel.app"
     ],
     allow_methods=["GET"],
